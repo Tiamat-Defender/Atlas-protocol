6714fd9120ffd70c9deff721f73dcf1eb665f161
Tiamat-Defender <pegreen130@gmail.com>
2024-12-18 16:56:10 -0500

    First commit

---
+++ AtlasPhoneOS.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+#define LORA_TX 0
+#define LORA_RX 1
+#define LORA_UART_NUMBER 0
+
+
+int main()
+{
+    stdio_init_all();
+
+    while (true) {
+        printf("Hello, world!\n");
+        sleep_ms(1000);
+    }
+}
---
+++ lora.h
@@ -0,0 +1,44 @@
+#include "pico/stdlib.h"
+#include "hardware/uart.h"
+#include <string.h>
+
+#define DEFBAUD 115200
+
+uint8_t index = 0;
+
+int INIT_LORA(uint8_t TX, uint8_t RX, uint8_t UART_TYPE)
+{
+    char buff[3];
+
+    gpio_set_function(TX, GPIO_FUNC_UART);
+    gpio_set_function(RX, GPIO_FUNC_UART);
+
+    if (UART_TYPE == 0)
+    {
+        uart_init(uart0, DEFBAUD);
+        uart_puts(uart0, "AT");
+
+        while (true)
+        {
+
+            if (uart_is_readable(uart0))
+            { 
+                    buff[index] = uart_getc(uart0);
+                    index ++;
+                    
+                    if(index >= 4)
+                    {
+
+                        if(strcmp(buff, "+ok"))return 0;
+                    }
+                
+            }
+        }
+    }else if (UART_TYPE == 1){   
+        uart_init(uart1, DEFBAUD);
+
+
+    }else return 1;
+    
+
+}
