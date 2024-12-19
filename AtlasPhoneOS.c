2358f8e2a2633b6b312732c5629dbe0bf76ee816
Tiamat-Defender <pegreen130@gmail.com>
2024-12-18 23:13:48 -0500

    In the morning  make full  RTOS implementation

--- AtlasPhoneOS.c
+++ AtlasPhoneOS.c
@@ -4,12 +4,39 @@
 #include "FreeRTOSConfig.h"
 #include "task.h"
 
+void fun1()
+{
+    gpio_init(0);
+    gpio_set_dir(0, GPIO_OUT);
+
+
+    while (true)
+    {
+        gpio_put(0, 1);
+        sleep_ms(100);
+        gpio_put(0, 0);
+        sleep_ms(100);          
+    }
+    
+
+}
 
 int main()
 {
     stdio_init_all();
 
+    TaskHandle_t task1 = NULL;
 
+    uint32_t status = xTaskCreate(
+                        fun1,
+                        "task1",
+                        1024,
+                        NULL,
+                        tskIDLE_PRIORITY,
+                        &task1
+    );
+
+    vTaskStartScheduler();
 
     return 0;
 }
