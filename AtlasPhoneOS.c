ab79ed1bb8f1a0ae2ad9a46d7da32aca272035e1
Tiamat-Defender <pegreen130@gmail.com>
2024-12-18 22:56:20 -0500

    Got FreeRTOS to work

--- AtlasPhoneOS.c
+++ AtlasPhoneOS.c
@@ -1,23 +1,15 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
 
 #include "FreeRTOS.h"
 #include "FreeRTOSConfig.h"
 #include "task.h"
 
-#define LORA_TX 0
-#define LORA_RX 1
-#define LORA_UART_NUMBER 0
-
 
 int main()
 {
-
     stdio_init_all();
-    while (true) {
-            printf("Hello, world!\n");
-            sleep_ms(1000);
-    }
-}
 
-    
+
+
+    return 0;
+}
