4aecfa34b69a8f093537e70ee79caf28a60619e4
Tiamat-Defender <pegreen130@gmail.com>
2024-12-19 22:30:30 -0500

    AY

--- AtlasPhoneOS.c
+++ AtlasPhoneOS.c
@@ -4,17 +4,30 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+
 #define PIN 0
 
-void LCD_INIT_TASK() {
+void LCD_STARTUP() {
     // Initialize I2C and LCD
     lcd_init();
-    lcd_set_cursor(1, 0);
-
+    //animation for startup will say atlas os then switch to code for a better future
+    lcd_set_cursor(0, 4);
     lcd_string("ATLAS OS");
+    sleep_ms(2000);
+    lcd_clear();
+    lcd_set_cursor(0, 3);
+    lcd_string("Code for a");
+    lcd_set_cursor(1, 1);
+    lcd_string("Better future.");
+}
 
-    vTaskDelete(NULL);
+
+void vMainMenu() {
+    lcd_clear();
     
+
+
+
 }
 
 void vBlinkTask() {
@@ -33,7 +46,7 @@ void main() {
     gpio_init(PIN);
     gpio_set_dir(PIN, GPIO_OUT);
 
-    xTaskCreate(LCD_INIT_TASK, "LCD Init", 128, NULL, 1, NULL);
+    LCD_STARTUP();
 
     xTaskCreate(vBlinkTask, "Blink Task", 128, NULL, 1, NULL);
     vTaskStartScheduler();
